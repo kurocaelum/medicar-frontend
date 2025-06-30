67fe4d47b8c6c90b128a3b1bafed1532ef46bc73
Victor <victor_brandao@outlook.com>
2025-06-30 20:29:42 -0300

    style: definição de cores do tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#49B4BB',
+        hover: '#D9F1F3'
+      },
+      secondary_hover: '#D9F1F3',
+      gray: {
+        DEFAULT: '#A8A8A8',
+        light: "#F8F8F8"
+      }
+    }
   },
   plugins: [],
 }
