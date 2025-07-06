acf510a2d50a005b80eb2284d2fae15577ea9afe
Alefe Oliveira <alefe.oliveira@logiquesistemas.com.br>
2025-07-06 10:04:00 -0300

    [LOGIN] - fix - correção para o acrescentar mat form field ao campo de login

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [
     LoginComponent
