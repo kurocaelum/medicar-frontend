456edc1889ae155a6f14d4c7ac4926f479fca246
Victor <victor_brandao@outlook.com>
2025-07-25 13:46:01 -0300

    [HOME] - feat - toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const MaterialComponents = [
   MatFormFieldModule,
   MatInputModule,
-  MatIconModule
+  MatIconModule,
+  MatToolbarModule
 ]
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { SharedModule } from '../shared/shared.module';
-import { MaterialModule } from '../material/material.module';
-import { SignupComponent } from './signup/signup.component';
 
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    RouterModule
   ],
   declarations: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent
   ],
   exports: [
     LoginComponent
